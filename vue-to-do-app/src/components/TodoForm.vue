10b7754c9bd81c1a55f0c410ab778ed046cb79c3
Volos Ivan <ivan.volos1@gmail.com>
2021-08-27 02:21:56 +0300

    Created basic vue.js project, added styles, inicial configuration

---
+++ vue-to-do-app/src/components/TodoForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="text" class="new-todo" placeholder="What needs to be done?">
+  </div>
+  <main>
+    <task-list></task-list>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'todo-form',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
